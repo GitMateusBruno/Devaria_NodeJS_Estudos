ecaf47131054048777fd9950e87de3aae2e7c804
Mateus Bruno <suportemateusbruno@gmail.com>
2023-03-08 21:10:07 -0300

    Aula dia 08/03 {html_css}

---
+++ AULA_13/app.js
@@ -0,0 +1,27 @@
+
+
+const x = null;
+let y
+
+console.log(x)
+console.log(y === undefined)
+
+////////////////////////////////////////////// [nunca inicie uma variavel com null]
+
+let a = ''
+let b = null
+let c = 34
+let d = ZZ
+
+console.log(let)
+
+///////////////////////////condicionais//////////////////
+
+let vel = 60.5
+
+console.log(`A velocidade de seu carro é ${vel} km/h`)
+
+if (vel > 50) {
+    console.log(`Você ultrapassou o limite de velocidade, otario`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
