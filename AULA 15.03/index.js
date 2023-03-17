cc52e0ee7396ac1aa68718bafb711e9ed6bbf684
Mateus Bruno <suportemateusbruno@gmail.com>
2023-03-16 23:11:22 -0300

    AULA 15.03

---
+++ AULA 15.03/index.js
@@ -0,0 +1,88 @@
+var texto1 = document.getElementById('texto1');   
+var visivel = false;
+
+var texto1 = document.getElementById('texto1');
+texto1.addEventListener('click', function() { 
+ if (!visivel) {
+   textoum.style.display = 'block';
+   visivel = true;
+ } else {
+   textoum.style.display = 'none';
+   visivel = false;
+ }   
+});
+
+
+ var texto2 = document.getElementById('texto2');   
+ var visivel = false;
+ 
+ var texto2 = document.getElementById('texto2');
+ texto2.addEventListener('click', function() { 
+  if (!visivel) {
+    textodois.style.display = 'block';
+    visivel = true;
+  } else {
+    textodois.style.display = 'none';
+    visivel = false;
+  }   
+ });
+
+
+ var texto3 = document.getElementById('texto3');   
+ var visivel = false;
+ 
+ var texto3 = document.getElementById('texto3');
+ texto3.addEventListener('click', function() { 
+  if (!visivel) {
+    textotres.style.display = 'block';
+    visivel = true;
+  } else {
+    textotres.style.display = 'none';
+    visivel = false;
+  }   
+ });
+
+
+ var texto4 = document.getElementById('texto4');   
+ var visivel = false;
+ 
+ var texto4 = document.getElementById('texto4');
+ texto4.addEventListener('click', function() { 
+  if (!visivel) {
+    textoquatro.style.display = 'block';
+    visivel = true;
+  } else {
+    textoquatro.style.display = 'none';
+    visivel = false;
+  }   
+ });
+
+
+ var texto5 = document.getElementById('texto5');   
+ var visivel = false;
+ 
+ var texto5 = document.getElementById('texto5');
+ texto5.addEventListener('click', function() { 
+  if (!visivel) {
+    textocinco.style.display = 'block';
+    visivel = true;
+  } else {
+    textocinco.style.display = 'none';
+    visivel = false;
+  }   
+ });
+
+
+ var texto6 = document.getElementById('texto6');   
+ var visivel = false;
+ 
+ var texto6 = document.getElementById('texto6');
+ texto6.addEventListener('click', function() { 
+  if (!visivel) {
+    textoseis.style.display = 'block';
+    visivel = true;
+  } else {
+    textoseis.style.display = 'none';
+    visivel = false;
+  }   
+ });
